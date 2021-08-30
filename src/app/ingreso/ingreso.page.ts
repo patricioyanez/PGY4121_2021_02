f71ff678ba1b536fc93519a2dacf760117de95a6
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2021-08-30 16:26:35 -0400

    promedio de notas con ponderación
    uso de metodos, ngIf, imprimir atributos, etc

--- src/app/ingreso/ingreso.page.ts
+++ src/app/ingreso/ingreso.page.ts
@@ -12,9 +12,20 @@ export class IngresoPage implements OnInit {
 
   ngOnInit() {
   }
-  promediar(nota1: HTMLInputElement,nota2: HTMLInputElement,nota3: HTMLInputElement)
+  promediar(nota1: HTMLInputElement,
+            nota2: HTMLInputElement,
+            nota3: HTMLInputElement,
+            pon1: HTMLInputElement,
+            pon2: HTMLInputElement,
+            pon3: HTMLInputElement)
   {
-    this.aux = Number(nota1.value) + Number(nota2.value) + Number(nota3.value);
-    this.promedio = this.aux / 3;
+    //this.aux = Number(nota1.value) + Number(nota2.value) + Number(nota3.value);
+    //this.promedio = this.aux / 3;
+    let n1 = Number(nota1.value) * Number(pon1.value) / 100;
+    let n2 = Number(nota2.value) * Number(pon2.value) / 100;
+    let n3 = Number(nota3.value) * Number(pon3.value) / 100;
+
+    this.promedio = n1 + n2 + n3;
+
   }
 }
